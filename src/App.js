d17d8c4dc40abd6310f74f804967d796642d7f61
Mayrla <mayrlasj@gmail.com>
2022-06-16 16:01:02 -0300

    feat(routes): criaçao de estrutura de rotas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import SearchResults from './pages/SearchResults';
+import RestaurantDetails from './pages/RestaurantDetails';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/search" exact element={<SearchResults />} />
+          <Route path="/details" exact element={<RestaurantDetails />} />
+          <Route path="*" exact element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+} 
 
 export default App;
---
+++ src/pages/RestaurantDetails/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RestaurantDetails(){
+  return(
+    <p>RestaurantDetails</p>
+  )
+}
+
+export default RestaurantDetails;
