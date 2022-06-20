657eb75831f43604a9f970905c9cb5e6843f9e14
Mayrla <mayrlasj@gmail.com>
2022-06-20 09:42:37 -0300

    fix(hook): melhoria na lógica do hook

--- src/hooks/restaurantsList.hook.js
+++ src/hooks/restaurantsList.hook.js
@@ -1,23 +1,11 @@
 import { useState, useEffect } from 'react';
 import api from '../api';
 
-export function useRestaurants(search, limit) {
+export function useRestaurants(search="", limit) {
   const [listRestaurants, setListRestaurants] = useState([])
   const [total, setTotal] = useState(0);
 
   useEffect(() => {
-    if(!search){
-      api
-      .get(`restaurants?page=1&limit=${limit}`)
-      .then((response) => {
-        console.log(response)
-        setListRestaurants(response?.data.data)
-        setTotal(response?.data.pagination.total)
-      })
-      .catch((err) => {
-        console.error('ops! ocorreu um erro' + err)
-      })
-    }else{
       api
         .get(`restaurants?page=1&limit=${limit}&search=${search}`)
         .then((response) => {
@@ -28,7 +16,6 @@ export function useRestaurants(search, limit) {
         .catch((err) => {
           console.error('ops! ocorreu um erro' + err)
         })
-    }
   }, [search, limit])
 
   return [listRestaurants, total]
